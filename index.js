ba8a556d72b6bba89a614b69705333701e9b3d26
rleenrwd <norwoodtheprogrammer@gmail.com>
2025-12-10 15:27:31 -0800

    Created a basic server template. Downloaded more dependencies.

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views')) // This says, look in the folder where this index.js file is and look for the views folder for my templates
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(PORT, () => {
+    console.log('The server is running on port: ', PORT)
+})
