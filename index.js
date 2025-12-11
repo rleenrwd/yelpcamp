449c043d26f34549ec681c1696081fddf288626e
rleenrwd <norwoodtheprogrammer@gmail.com>
2025-12-10 18:56:14 -0800

    Separated the campground route logic into a separate file inside a routes folder.

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const methodOverride = require('method-override')
-const Campground = require('./models/campground.js')
+const campgroundRoutes = require('./routes/campgrounds.js')
 const connectDB = require('./db.js')
-const { runInNewContext } = require('vm')
+
 
 
 connectDB()
@@ -20,46 +20,12 @@ app.set('views', path.join(__dirname, 'views')) // This says, look in the folder
 app.use(express.urlencoded({extended: true})) // This is used for HTML form submissions to parse the data into req.body
 app.use(methodOverride('_method'))
 
+app.use('/campgrounds', campgroundRoutes)
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({})
-    res.render('campgrounds/index', {campgrounds})
-})
-
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new')
-})
-
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground)
-    await campground.save()
-    res.redirect(`/campgrounds/${campground._id}`)
-})
-
-app.get('/campgrounds/:id', async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/show', {campground})
-})
-
-app.get('/campgrounds/:id/edit', async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/edit', {campground})
-})
-
-app.put('/campgrounds/:id', async (req, res) => {
-    const {id} = req.params
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
-    res.redirect(`/campgrounds/${campground._id}`)
-})
-
-app.delete('/campgrounds/:id', async (req, res) => {
-    const {id} = req.params
-    await Campground.findByIdAndDelete(id)
-    res.redirect('/campgrounds')
-})
 
 app.listen(PORT, () => {
     console.log('The server is running on port: ', PORT)
---
+++ routes/campgrounds.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const router = express.Router()
+const Campground = require('../models/campground')
+
+// INDEX
+router.get('/', async (req, res) => {
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index', {campgrounds})
+})
+
+// NEW CAMPGROUND FORM
+router.get('/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+// CREATE CAMPGROUND ROUTE
+router.post('/', async (req, res) => {
+    const campground = new Campground(req.body.campground)
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+// SHOW ROUTE
+router.get('/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', {campground})
+})
+
+// EDIT FORM
+router.get('/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', {campground})
+})
+
+// UPDATE CAMPGROUND ROUTE
+router.put('/:id', async (req, res) => {
+    const {id} = req.params
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+// DELETE CAMPGROUND ROUTE
+router.delete('/:id', async (req, res) => {
+    const {id} = req.params
+    await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
+})
+
+module.exports = router
