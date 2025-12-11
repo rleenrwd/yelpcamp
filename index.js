1d7457671ef990de97efee7efd172c58678c3b18
rleenrwd <norwoodtheprogrammer@gmail.com>
2025-12-10 18:41:50 -0800

    Installed methodoverride. Created edit.ejs file. Created put and delete routes for campgrounds. Basic CRUD for campgrounds done.

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const methodOverride = require('method-override')
 const Campground = require('./models/campground.js')
 const connectDB = require('./db.js')
+const { runInNewContext } = require('vm')
 
 
 connectDB()
@@ -16,6 +18,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views')) // This says, look in the folder where this index.js file is and look for the views folder for my templates
 
 app.use(express.urlencoded({extended: true})) // This is used for HTML form submissions to parse the data into req.body
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -41,6 +44,22 @@ app.get('/campgrounds/:id', async (req, res) => {
     res.render('campgrounds/show', {campground})
 })
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', {campground})
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params
+    await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
+})
 
 app.listen(PORT, () => {
     console.log('The server is running on port: ', PORT)
