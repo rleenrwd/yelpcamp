7c2d87a56a4fb801c0adc2f3de46d7f5fb3fa3b4
rleenrwd <norwoodtheprogrammer@gmail.com>
2025-12-10 16:10:47 -0800

    Refactored the db.js file.

--- db.js
+++ db.js
@@ -4,18 +4,19 @@ require('dotenv').config()
 const DB_URI = process.env.DB_URI || 'mongodb://127.0.0.1:27017/yelpcamp'
 const connection = mongoose.connection
 
-connection.on('connected', () => console.log('The database is now connected!'))
-connection.on('open', () => console.log('The database is now open!'))
+const handleError = (err) => {
+    console.error('There was a problem connecting to the database: ', err)
+    process.exit(1)
+}
+
+connection.on('connected', () => console.log('MongoDB connected!'))
 connection.on('disconnected', () => console.log('The database is now disconnected.'))
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(DB_URI, {
-            serverSelectionTimeoutMS: 5000
-        })
+        await mongoose.connect(DB_URI)
     } catch (err) {
-        console.error('There was a problem connecting to the database: ', err)
-        process.exit(1)
+        handleError(err)
     }
 }
 
